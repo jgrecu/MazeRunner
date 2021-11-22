ff2a6157ebc261b229ebd0425f082f635dcebf6c
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-22 13:33:40 +0000

    JBA - Maze Runner - Stage 1/4: Display a maze

--- task/src/maze/Main.java
+++ task/src/maze/Main.java
@@ -2,6 +2,44 @@ package maze;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[][] maze = createMaze();
+        printMaze(maze);
+    }
+
+    private static int[][] createMaze() {
+        int[][] array;
+
+        array = new int[][]{
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+                {1, 0, 1, 0, 0, 0, 1, 0, 1, 1},
+                {1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0, 1, 1, 0, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0, 1, 1},
+                {1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 1},
+                {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+//        Random rand = new Random();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[0].length; j++) {
+//                if (i == 0 || i == 9) {
+//                    array[i][j] = 1;
+//                } else {
+//                    array[i][j] = rand.nextInt(2);
+//                }
+//            }
+//        }
+        return array;
+    }
+
+    private static void printMaze(int[][] maze) {
+        for (int[] row : maze) {
+            for (int cell : row) {
+                System.out.print(cell == 1 ? "\u2588\u2588":"  ");
+            }
+            System.out.println();
+        }
     }
 }
